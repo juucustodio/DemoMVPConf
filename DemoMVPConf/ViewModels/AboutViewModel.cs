5ae654041785fb5766961d9cd078410513a67325
Juliano Custódio <juliano.custodio@hotmail.com.br>
2019-04-12 16:42:00 -0300

    Demo MVPConf

---
+++ DemoMVPConf/ViewModels/AboutViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Input;
+
+using Xamarin.Forms;
+
+namespace DemoMVPConf.ViewModels
+{
+    public class AboutViewModel : BaseViewModel
+    {
+        public AboutViewModel()
+        {
+            Title = "About";
+
+            OpenWebCommand = new Command(() => Device.OpenUri(new Uri("https://mvpconf.com.br")));
+        }
+
+        public ICommand OpenWebCommand { get; }
+    }
+}
